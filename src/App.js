098eff96cb13de807c6cec75bbf147c3dbf04bf5
funbunch <shannan_bunch@hotmail.com>
2022-04-23 17:27:38 -0700

    Adding to 1.3

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+const App = () => {
+  const course = 'Half Stack app dev'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 =  {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 =  {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}  </p>
+    </div>
+  )
+}
+
+export default App
